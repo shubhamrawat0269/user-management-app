ebc6294b888ea0094eeae4aa8046825e1454a0cd
shubham Rawat <rawatshubam269@gmail.com>
2024-07-28 21:51:39 +0530

    dbURL constraint change

--- server/db/connectToDb.js
+++ server/db/connectToDb.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectToDb = async () => {
-  const dBURL = process.env.DB_URI || `mongodb://localhost:27017/users`;
+  const dBURL = process.env.DB_URI || `mongodb://127.0.0.1:27017/users`;
   try {
     console.log(
       "MongoDB URI:",
