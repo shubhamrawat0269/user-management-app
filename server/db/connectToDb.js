a1f8d22d29eb8fa1facd2590767b5e64de6d1785
shubham Rawat <rawatshubam269@gmail.com>
2024-07-29 09:26:48 +0530

    more changes

--- server/db/connectToDb.js
+++ server/db/connectToDb.js
@@ -4,7 +4,9 @@ const connectToDb = async () => {
   const dBURL = process.env.DB_URI;
 
   try {
-    await mongoose.connect(`${dBURL}`);
+    await mongoose.connect(
+      "mongodb+srv://admin:admin@usercluster.tmocjim.mongodb.net/users?retryWrites=true&w=majority&appName=userCluster"
+    );
     console.log(`Connected to DB at ${dBURL}`);
   } catch (error) {
     console.log(`Failed to connect to MongoDB`, error);
