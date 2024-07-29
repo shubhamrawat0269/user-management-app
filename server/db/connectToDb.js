e882a593f8dddf9ec3b7dfbd0e03c74a51910374
shubham Rawat <rawatshubam269@gmail.com>
2024-07-29 09:00:38 +0530

    try to fix mongodb error

--- server/db/connectToDb.js
+++ server/db/connectToDb.js
@@ -2,8 +2,9 @@ const mongoose = require("mongoose");
 
 const connectToDb = async () => {
   const dBURL = process.env.DB_URI;
+
   try {
-    await mongoose.connect(dBURL);
+    await mongoose.connect(`${dBURL}`);
     console.log(`Connected to DB at ${dBURL}`);
   } catch (error) {
     console.log(`Failed to connect to MongoDB`, error);
